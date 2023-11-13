a8273ee6c5f34052942d31a6cf2c60d017d8e873
luishbarros <luishrbr@outlook.com>
2023-11-13 18:23:44 -0300

    Banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
 import fastify from "fastify";
-
+import { knex } from "./database";
+import crypto from "node:crypto";
+import { env } from "./env";
 const app = fastify();
 
-app.get("/hello", () => {
-    return "Hello, World";
+app.get("/hello", async () => {
+  const transactions = await knex("transactions")
+    .insert({
+      id: crypto.randomUUID(),
+      title: "Test Transaction",
+      amount: 1000,
+    })
+    .returning("*");
+  return transactions;
 });
 
 app
-    .listen({ port: 3000 })
-    .then(() => console.log("Sever running at port 3333"));
+  .listen({ port: env.PORT })
+  .then(() => console.log("Sever running at port 3333"));
+
+
